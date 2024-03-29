f6b4d407ae9893cd6c0210e3546515723b678288
luoyan <luo.yan@neusoft.com>
2024-03-29 10:57:51 +0800

    删除

--- RuoYi-Vue-fast/src/main/java/com/ruoyi/project/medicines/service/MedicinesService.java
+++
@@ -1,7 +0,0 @@
-package com.ruoyi.project.medicines.service;
-
-import com.ruoyi.project.medicines.domain.Medicines;
-
-public interface MedicinesService {
-    int addMedicines(Medicines medicines);
-}
--- RuoYi-Vue3/src/api/medicines/medicines.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function addMedicines(data) {
-    return request({
-        url: '/medicines/addMedicines',
-        method: 'post',
-        data: data
-    })
-}
